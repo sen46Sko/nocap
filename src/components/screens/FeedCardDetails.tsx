816e17db62ce66ab5aabe5136b2ba3ac2e4e0af2
VysochanskiyS <visosensey@gmail.com>
2024-04-25 14:57:06 +0300

    Done the feed card details screen

--- src/components/screens/FeedCardDetails.tsx
+++ src/components/screens/FeedCardDetails.tsx
@@ -10,7 +10,7 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
 
 import {RootStackParamList, Screens} from 'utils/types/navigation';
-import {Menu, Share} from 'assets/images';
+import {Calendar, Locaiton, Menu, Phone, Share} from 'assets/images';
 
 type Props = NativeStackScreenProps<
   RootStackParamList,
@@ -21,14 +21,14 @@ export const FeedCardDetails: React.FC<Props> = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
+        <View className="gap-[16px]">
           <View className="px-[10px] py-[16px]">
             <Text className=" font-robotoMedium color-grayMedium text-[24px]">
               nocap
             </Text>
           </View>
 
-          <View className="px-[10px] py-[14px]">
+          <View className="px-[10px]">
             <Text className=" font-robotoBold color-white text-[16px]">
               Name
             </Text>
@@ -41,7 +41,7 @@ export const FeedCardDetails: React.FC<Props> = () => {
             className="w-full h-[516px]"
           />
 
-          <View className="p-[16px] flex-row items-center justify-between">
+          <View className="px-[6px] flex-row items-center justify-between">
             <View className="flex-row items-center gap-[8px]">
               <View className="bg-grayDark h-[32px] w-[32px] rounded-full items-center justify-center">
                 <Text className="text-[20px]">😍</Text>
@@ -52,6 +52,36 @@ export const FeedCardDetails: React.FC<Props> = () => {
             <View className="flex-row gap-[24px] items-center">
               <Share />
               <Menu />
+            </View>
+          </View>
+
+          <View className="flex-row px-[6px]">
+            <Text className="font-robotoRegular color-white">
+              Girls pose at Maintown
+            </Text>
+            <Text className="font-robotoRegular color-grayMedium">...more</Text>
+          </View>
+
+          <View className="px-[6px] gap-[10px] mt-[8px]">
+            <View className="flex-row items-center">
+              <Locaiton />
+              <Text className="font-robotoRegular color-grayMedium text-[12px]">
+                Singapore
+              </Text>
+            </View>
+
+            <View className="flex-row items-center">
+              <Calendar />
+              <Text className="font-robotoRegular color-grayMedium text-[12px]">
+                12 February 2024, 12:04 pm
+              </Text>
+            </View>
+
+            <View className="flex-row items-center">
+              <Phone />
+              <Text className="font-robotoRegular color-grayMedium text-[12px]">
+                Phone 15 Pro Max
+              </Text>
             </View>
           </View>
         </View>
