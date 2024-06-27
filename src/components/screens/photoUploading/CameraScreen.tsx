e95771bc4ab73a328dd47f1476c2b5e75b466618
Sergiy Vysochanskyi <visosensey@gmail.com>
2024-06-27 16:40:45 +0300

    Add more icons, implement flash, rotate photos from the front camera

--- src/components/screens/photoUploading/CameraScreen.tsx
+++ src/components/screens/photoUploading/CameraScreen.tsx
@@ -1,11 +1,12 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {Camera, CameraType, FlashMode} from 'expo-camera';
+import {FlipType, manipulateAsync} from 'expo-image-manipulator';
 import {Pressable, SafeAreaView, StyleSheet, View} from 'react-native';
 import React, {useRef, useState} from 'react';
 
 import {RootStackParamList, Screens} from 'utils/types/navigation';
 import {screenWidth} from 'utils/helpers';
-import {Expand, Selfie} from 'assets/images';
+import {Expand, Flash, FlashAuto, FlashOff, Selfie, Settings, Timer10, Timer3, Timer5, TimerOff} from 'assets/images';
 import {useIsFocused} from '@react-navigation/native';
 import {If} from 'components/atoms/If';
 
@@ -14,6 +15,7 @@ type Props = NativeStackScreenProps<RootStackParamList, Screens.CAMERA_SCREEN>;
 export const CameraScreen: React.FC<Props> = ({navigation}) => {
   const [cameraType, setCameraType] = useState(CameraType.back);
   const [flashMode, setFlashMode] = useState(FlashMode.off);
+  const [timer, setTimer] = useState(0);
 
   const cameraRef = useRef<Camera>(null);
 
@@ -29,20 +31,95 @@ export const CameraScreen: React.FC<Props> = ({navigation}) => {
     });
   };
 
-  const takePicture = () => {
-    cameraRef.current
-      ?.takePictureAsync()
-      .then(res =>
-        navigation.navigate(Screens.IMAGE_PREVIEW, {image: res?.uri || ''}),
-      );
+  const toggleFlash = () => {
+    setFlashMode(current => {
+      if (current === FlashMode.on) {
+        return FlashMode.auto;
+      }
+      if (current === FlashMode.auto) {
+        return FlashMode.off;
+      }
+
+      return FlashMode.on;
+    });
+  };
+
+  const toggleTimer = () => {
+    setTimer(current => {
+      if (current === 0) {
+        return 3;
+      }
+      
+      if (current === 3) {
+        return 5;
+      }
+
+      if (current === 5) {
+        return 10;
+      }
+
+      return 0;
+    });
+  };
+
+  const takePicture = async () => {
+    let photo = await cameraRef.current?.takePictureAsync();
+
+    if (!photo) {
+      return;
+    }
+
+    if (cameraType === CameraType.front) {
+      photo = await manipulateAsync(
+        photo.uri,
+        [
+            { rotate: 180 },
+            { flip: FlipType.Vertical },
+        ],
+    );
+    }
+
+    navigation.navigate(Screens.IMAGE_PREVIEW, {image: photo?.uri})
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View className="py-[12px] flex-row justify-center items-center">
+      <View className="py-[12px] px-[16px] flex-row justify-center items-center relative">
         <Pressable onPress={navigation.goBack}>
           <Expand />
         </Pressable>
+        <View style={styles.options}>
+          <Pressable onPress={toggleFlash}>
+            <If condition={flashMode === FlashMode.on}>
+              <Flash />
+            </If>
+            <If condition={flashMode === FlashMode.off}>
+              <FlashOff />
+            </If>
+            <If condition={flashMode === FlashMode.auto}>
+              <FlashAuto />
+            </If>
+          </Pressable>
+
+          <Pressable onPress={toggleTimer}>
+            <If condition={timer === 0}>
+              <TimerOff />
+            </If>
+            <If condition={timer === 3}>
+              <Timer3 />
+            </If>
+            <If condition={timer === 5}>
+              <Timer5 />
+            </If>
+            <If condition={timer === 10}>
+              <Timer10 />
+            </If>
+          </Pressable>
+
+          <Pressable>
+            <Settings />
+          </Pressable>
+        </View>
       </View>
 
       <If condition={isFocused}>
@@ -78,5 +155,11 @@ const styles = StyleSheet.create({
   cameraView: {
     width: screenWidth,
     height: (screenWidth / 3) * 4,
+  },
+  options: {
+    position: 'absolute',
+    right: 16,
+    flexDirection: 'row',
+    gap: 24,
   },
 });
